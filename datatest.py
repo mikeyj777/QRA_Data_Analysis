b90eb2d535bd032fc159752d8f5fc99b377474f4
Michael James <u773253@emn.com>
2019-11-06 14:17:00 -0500

    sanitized notebook

--- datatest.py
+++ datatest.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#number of scenarios to generate data for.
 n=100
 
-#get random frequencies of loss of containment scenarios (bet 1e-10 and 1e-5)
-frequencies = -np.random.randint(low=1, high=500000, size=n)/1e10
+#get random frequencies of loss of containment scenarios (bet 1e-12 and 5e-7)
+frequencies = np.random.randint(low=1, high=500000, size=n)/1e12
 
 
 #get a normal distribution of probabilities of injury to associate with a loss of containment.  project between 0 and 1.
@@ -41,6 +42,8 @@ ax.plot([1, 10, 1000], [0.01, 0.001, 0.00001], c = 'red')
 ax.plot([1, 10, 1000], [0.0001, 0.00001, 0.0000001], c = 'green')
 ax.set_yscale('log')
 ax.set_xscale('log')
+plt.xlabel('N or More Injuries')
+plt.ylabel('Cumulative Frequency')
 
 plt.show()
 
